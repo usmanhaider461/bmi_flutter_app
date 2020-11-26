d01f302be8cc7d20ec47481adee17d997b8c7f96
usmanhaider461 <46385291+usmanhaider461@users.noreply.github.com>
2020-11-26 08:54:57 +0500

    Basic Structure of the theme

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -8,21 +9,76 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI Calculator'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: Theme(
-        data: ThemeData(
-          accentColor: Colors.purpleAccent,
+        appBar: AppBar(
+          title: Text('BMI Calculator'),
         ),
-        child: FloatingActionButton(
-          child: Icon(Icons.add),
+        floatingActionButton: Theme(
+          data: ThemeData(
+            accentColor: Colors.purpleAccent,
+          ),
+          child: FloatingActionButton(
+            child: Icon(Icons.add),
+          ),
         ),
-      ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
 
-    );
+                    child: Container(
+                      margin: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                          color: Color(0xff1d1e33),
+                          borderRadius: BorderRadius.circular(10.0)),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                          color: Color(0xff1d1e33),
+                          borderRadius: BorderRadius.circular(10.0)),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+
+              child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                    color: Color(0xff1d1e33),
+                    borderRadius: BorderRadius.circular(10.0)),
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+
+                    child: Container(
+                      margin: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                          color: Color(0xff1d1e33),
+                          borderRadius: BorderRadius.circular(10.0)),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                          color: Color(0xff1d1e33),
+                          borderRadius: BorderRadius.circular(10.0)),
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+          ],
+        ));
   }
 }
