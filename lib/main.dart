b621203f8e8db94324abf89d5ef45814261809a7
usmanhaider461 <46385291+usmanhaider461@users.noreply.github.com>
2020-11-26 08:30:36 +0500

    First ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,13 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      theme: ThemeData(
+        primaryColor: Color(0xff0a0e21),
+        scaffoldBackgroundColor: Color(0xff0a0e21),
+        // ignore: deprecated_member_use
+        textTheme: TextTheme(body1: TextStyle(color: Colors.white)),
+        accentColor: Colors.purpleAccent,
+      ),
       home: InputPage(),
     );
   }
