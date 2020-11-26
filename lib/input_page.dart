34ff36473486e4d0e86f2b0935f679ae1e2c8740
usmanhaider461 <46385291+usmanhaider461@users.noreply.github.com>
2020-11-26 08:36:24 +0500

    Create individual page InputPage is added

---
+++ lib/input_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+      floatingActionButton: Theme(
+        data: ThemeData(
+          accentColor: Colors.purpleAccent,
+        ),
+        child: FloatingActionButton(
+          child: Icon(Icons.add),
+        ),
+      ),
+
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
 void main() {
   runApp(BMICalculator());
@@ -19,25 +20,3 @@ class BMICalculator extends StatelessWidget {
   }
 }
 
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI Calculator'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-        ),
-
-    );
-  }
-}
