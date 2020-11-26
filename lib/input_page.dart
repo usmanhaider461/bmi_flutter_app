c77b53d72b3b15b69058298212049e4fb872d2b3
usmanhaider461 <46385291+usmanhaider461@users.noreply.github.com>
2020-11-26 09:42:43 +0500

    Card widget is added

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -57,11 +57,13 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.colour});
+  ReusableCard({@required this.colour, this.cardChild});
   final Color colour;
+  final Widget cardChild;
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
           color: colour, borderRadius: BorderRadius.circular(10.0)),
